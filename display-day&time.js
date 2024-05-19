06360acf80b0b85210e6f954faf6961fff62f2aa
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-05-19 13:21:44 +0600

    display day & time completed

--- display-day&time.js
+++ display-day&time.js
@@ -1,26 +1,34 @@
 // find the name of today/ weekday
-const today = new Date();
-const weekday  = today.getDay();
-console.log(weekday);
+let today = new Date();
+let day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+day = day[today.getDay()];
+// console.log(day);
+const displayer1 = document.getElementById('today');
+displayer1.innerHTML = day;
+
 // knowledge : 
 // 0=sunday 1=monday 2=tuesday 3=wednesday 4= thursday 5=friday 6=satuday
 
 // from numeric value - try to get the name of the weekday :Home Work
 
 //display the numeric value of the weekday
-const displayer = document.getElementById('today')
+// const displayer = document.getElementById('today')
 // solution 1: using innerHTML
 /////displayer.innerHTML = weekday
 // solution 2
 /// make displayer empty
 //solution 1: to make empty
- while(displayer.firstChild){
-  displayer.removeChild(displayer.firstChild)
-}  
+// while (displayer.firstChild) {
+//   displayer.removeChild(displayer.firstChild)
+// }
 ///solution 2L to make empty
 // displayer.innerHTML = ''
-const txtNode = document.createTextNode(6);
-displayer.appendChild(txtNode);
+// const txtNode = document.createTextNode(6);
+// displayer.appendChild(txtNode);
+
+//display the day name of the weekday
+
 
 /* for(child of displayer.childNodes){
   displayer.removeChild(child)
@@ -31,9 +39,19 @@ displayer.appendChild(txtNode);
 // find the current time
 
 // knowledge - 10PM 22
-let hours = today.getHours()
-let minutes = today.getMinutes()
-let seconds = today.getSeconds()
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+  return strTime;
+}
 
-let currentTimne = hours + ":" + minutes + ":" + seconds
-console.log(currentTimne)
+// console.log(formatAMPM(new Date));
+let getTime = document.getElementById('time');
+getTime.innerHTML = formatAMPM(new Date);
