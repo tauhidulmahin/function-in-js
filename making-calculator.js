139f64f48690466a696f15716b71bd195e8f3b36
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-05-19 10:57:34 +0600

    making calculator file added

---
+++ making-calculator.js
@@ -0,0 +1,32 @@
+// screen to display
+let screen = document.getElementById("screen");
+
+// display digit from 0 to 9
+let buttonPad = document.getElementById("buttonPad");
+let txtNode = null;
+let btn = null;
+let counter = 3;
+
+for (i = 0; i < 10; i++) {
+
+    // i expect to show mutton 3x3 column
+
+    if (i < counter) {
+
+        txtNode = document.createTextNode(i);
+        btn = document.createElement("BUTTON");
+        btn.appendChild(txtNode);
+        buttonPad.appendChild(btn);
+
+    } else {
+        br = document.createElement("BR");
+        buttonPad.appendChild(br);
+
+        txtNode = document.createTextNode(i);
+        btn = document.createElement("BUTTON");
+        btn.appendChild(txtNode);
+        buttonPad.appendChild(btn);
+
+        counter = counter + 3;
+    }
+}
